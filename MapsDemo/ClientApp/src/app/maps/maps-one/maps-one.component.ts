8cc1ef77909394ff3de738fe5c9a6ea4a1136926
unknown <bloxofcodes@gmail.com>
2023-06-15 16:50:38 +0800

    Maps Demo for Angular&.NETCore6

---
+++ MapsDemo/ClientApp/src/app/maps/maps-one/maps-one.component.ts
@@ -0,0 +1,23 @@
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+
+@Component({
+  selector: 'app-maps-one',
+  templateUrl: './maps-one.component.html',
+  styleUrls: ['./maps-one.component.css']
+})
+
+export class MapsOneComponent implements AfterViewInit {
+
+  imgMap1: string = "/assets/images/sg-map-1-gray.png";
+  imgMap2: string = "/assets/images/sg-map-2-gray.png";
+
+  pin1: string = "/assets/images/pin.png";
+
+  ngAfterViewInit() {
+
+
+  }
+
+
+}
